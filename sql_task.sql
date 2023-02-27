089c1675ff33220ede580497340d5b658b6d3d64
eachabys <44489101+eachabys@users.noreply.github.com>
2023-02-27 07:03:19 -0600

    Add files via upload

---
+++ sql_task.sql
@@ -0,0 +1,3 @@
+create table customer(customer_id int, first_name varchar(20),last_name varchar(20),email varchar(20),phone int);
+insert into customer values(2023,"Elmira","F.","ea@gmail.com",832641);
+select * from customer;
